d0d66531dfd5776988e5822cd3dee995054e3e99
Oleg Suvorov <suvorov_oleg@mail.ru>
2023-12-07 01:46:59 +0300

    2 steps

--- test_main.py
+++ test_main.py
@@ -11,29 +11,44 @@ class TelegramUpdate(BaseModel):
 
 app = FastAPI()
 
-quest_state=-2
-quest_answers=[5,5,5,5,5,5,5,5,5,5,5,5]
-STATE_NAME=-2
-STATE_GROUP=-1
-STATE_Q1=0
-STATE_Q2=1
-STATE_Q3=2
-STATE_Q4=3
-STATE_Q5=4
-STATE_Q6=5
-STATE_Q7=6
-STATE_Q8=7
-STATE_Q9=8
-STATE_Q10=9
-STATE_Q11=10
-STATE_Q12=11
-student_name=""
+
+(
+	NM,
+	
+	GRP,
+
+    Q1,
+
+    Q2,
+
+    Q3,
+
+    Q4,
+
+    Q5,
+
+    Q6,
+
+    Q7,
+
+    Q8,
+
+    Q9,
+
+    Q10,
+
+    Q11,
+
+    Q12,
+
+) = map(chr, range(10, 24))
+
 
 # Load variables from .env file if present
 load_dotenv()
 
 # Read the variable from the environment (or .env file)
-bot_token = os.getenv('BOT_TOKEN')
+bot_token = "6938936654:AAFsbROhCJk3epwX_vVQgp6XqoLP2-JL9iw"
 secret_token = os.getenv("SECRET_TOKEN")
 # webhook_url = os.getenv('CYCLIC_URL', 'http://localhost:8181') + "/webhook/"
 
@@ -44,8 +59,8 @@ bot = Bot(token=bot_token)
 
 def auth_telegram_token(x_telegram_bot_api_secret_token: str = Header(None)) -> str:
     # return true # uncomment to disable authentication
-    if x_telegram_bot_api_secret_token != secret_token:
-        raise HTTPException(status_code=403, detail="Not authenticated")
+   # if x_telegram_bot_api_secret_token != secret_token:
+    #    raise HTTPException(status_code=403, detail="Not authenticated")
     return x_telegram_bot_api_secret_token
 
 @app.post("/webhook/")
@@ -56,16 +71,10 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
     # print("Received message:", update.message)
 
     if text == "/start":
-		quest_state=STATE_NAME
         #with open('hello.gif', 'rb') as photo:
             #await bot.send_photo(chat_id=chat_id, photo=photo)
-        await bot.send_message(chat_id=chat_id, text="2 аттестация. Пересдача.\n Введите фамилию.")
+        await bot.send_message(chat_id=chat_id, text="2 аттестация. Пересдача.\n Введите фамилию и группу.")
     else:
-		quest_state==quest_state+1
-		if quest_state==STATE_GROUP:
-			student_name=text
-			await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=student_name)
-		else:
-			await bot.send_message(chat_id=chat_id, text="Some problem")
+        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
 
     return {"ok": True}
