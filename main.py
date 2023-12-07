6f7195f0400c8e2700a9dbc2b20a85a53d41d5b0
Oleg Suvorov <suvorov_oleg@mail.ru>
2023-12-07 06:24:49 +0300

    user?

--- main.py
+++ main.py
@@ -182,10 +182,10 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
             result_string=result_string+usname
             print (result_string)
             result_bytes=result_string.encode('utf-8')
-            encoded_bytes=base64.b64encode(result_bytes)
+            encoded_bytes=base64.b16encode(result_bytes)
             encoded_string = encoded_bytes.decode('utf-8')
             #encoded_string = base64.b64encode(result_string.encode("utf-8")).decode("utf-8")
             print (encoded_string)
-            await bot.send_message(chat_id=chat_id, text="Done: "+encoded_string)
+            await bot.send_message(chat_id=chat_id, text="Тест завершён. Пожалуйста, скопируйте и отправьте со своего почтового ящика текст в квадратных скобках: ["+encoded_string+"]")
 
     return {"ok": True}
